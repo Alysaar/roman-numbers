6c544bf07ed00c4775f725e111735b9ebef2e6a6
Valentin Prulière <valentin.pruliere@soprasteria.com>
2021-06-10 16:26:11 +0200

    Commit initial

---
+++ TempSolution.Tests/RomanNumerals.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace TempSolution.Tests
+{
+    public class RomanNumerals
+    {
+        /// <summary>
+        /// Retourne le nombre associé au chiffres romains.
+        /// </summary>
+        /// <param name="romanNumber">Nombre sous forme de chiffres romains.</param>
+        /// <returns>Le nombre associé au chiffres romains.</returns>
+        public int Parse(string romanNumber)
+        {
+            return romanNumber switch
+            {
+                "I" => 1,
+                "II" => 2,
+                "III" => 3,
+                "IV" => 4, 
+                "V" => 5,
+                "VI" => 6,
+                "VII" => 7,
+                "VIII" => 8,
+                "IX" => 9,
+                "X" => 10,
+                "XI" => 11,
+                _ => throw new NotImplementedException()
+            };
+
+            // On peut donc voir I ==> 1 
+            // V ==> 5 
+            // X ==> 10
+            // mots de 4 lettres max
+
+
+        }
+    }
+}
---
+++ TempSolution.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace TempSolution.Tests
+{
+    public class RomanNumeralsTests
+    {
+        [Fact]
+        public void UnitesRomainesShouldReturnUnites()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+            List<string> unitesRomaines = new List<string>()
+            {
+                "I",
+                "II",
+                "III",
+                "IV",
+                "V",
+                "VI",
+                "VII",
+                "VIII",
+                "IX"
+            };
+            List<int> resultats = new List<int>();
+            List<int> expected = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            };
+
+
+            // Act (When)
+            foreach (string uniteRomaine in unitesRomaines)
+            {
+                resultats.Add(rn.Parse(uniteRomaine));
+            };
+
+            // Assert (Then)
+            Assert.Equal(expected, resultats);
+        }
+
+        [Fact]
+        public void IIShouldReturn2()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("II");
+
+            // Assert (Then)
+            Assert.Equal(2, number);
+        }
+
+        [Fact]
+        public void IIIShouldReturn3()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("III");
+
+            // Assert (Then)
+            Assert.Equal(3, number);
+        }
+
+        [Fact]
+        public void IVShouldReturn4()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("IV");
+
+            // Assert (Then)
+            Assert.Equal(4, number);
+        }
+
+        [Fact]
+        public void VShouldReturn5()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("V");
+
+            // Assert (Then)
+            Assert.Equal(5, number);
+        }
+
+        [Fact]
+        public void VIShouldReturn6()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("VI");
+
+            // Assert (Then)
+            Assert.Equal(6, number);
+        }
+
+        [Fact]
+        public void VIIShouldReturn7()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("VII");
+
+            // Assert (Then)
+            Assert.Equal(7, number);
+        }
+
+        [Fact]
+        public void VIIIShouldReturn8()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("VIII");
+
+            // Assert (Then)
+            Assert.Equal(8, number);
+        }
+
+        [Fact]
+        public void IXShouldReturn9()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("IX");
+
+            // Assert (Then)
+            Assert.Equal(9, number);
+        }
+
+        [Fact]
+        public void XShouldReturn10()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("X");
+
+            // Assert (Then)
+            Assert.Equal(10, number);
+        }
+
+        [Fact]
+        public void XIShouldReturn11()
+        {
+            // Arrange (Given)
+            RomanNumerals rn = new RomanNumerals();
+
+            // Act (When)
+            int number = rn.Parse("XI");
+
+            // Assert (Then)
+            Assert.Equal(11, number);
+        }
+    }
+}
